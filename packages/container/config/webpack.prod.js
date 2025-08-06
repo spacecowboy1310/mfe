c8bb33fdd9c22707e4a8b2863893fe8ea2cdb7d7
spacecowboy1310 <oier1310@gmail.com>
2025-08-06 10:41:15 +0200

    uncomment

--- packages/container/config/webpack.prod.js
+++ packages/container/config/webpack.prod.js
@@ -17,7 +17,7 @@ const prodConfig = {
             filename: 'remoteEntry.js',
             remotes: {
                 marketing: `marketing@${domain}/marketing/latest/remoteEntry.js`,
-                // auth: `auth@${domain}/auth/latest/remoteEntry.js`,
+                auth: `auth@${domain}/auth/latest/remoteEntry.js`,
                 // dashboard: `dashboard@${domain}/dashboard/latest/remoteEntry.js`,
             },
             shared: packageJson.dependencies,
